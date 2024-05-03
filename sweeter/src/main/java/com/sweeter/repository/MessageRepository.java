3a1b751f5eff26488bb46c6ea9afed4a84cfdeed
George-Lupalenko <134298193+George-Lupalenko@users.noreply.github.com>
2024-05-03 12:59:45 +0200

    Add files via upload

---
+++ sweeter/src/main/java/com/sweeter/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.sweeter.repository;
+import com.sweeter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message , Long > {
+    List<Message> findByTag(String tag);
+}
